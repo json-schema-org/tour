c712049e1479fe2db42a99100022df09d6dd93e1
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-05-09 14:10:20 +0530

    Refactor layout and page components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+// import { Providers } from "./providers";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+const font = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tour of JSON Schema",
-  description: "Learn JSON Schema by example with this interactive tour.",
+  title: "Tour Of Json Schema",
+  description: "A Tour of Json Schema",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>
+        {/* <Providers> */}
+        <main>{children}</main>
+        {/* </Providers> */}
+      </body>
     </html>
   );
 }
