c366f9d6cab765133393a54a24b351a36feb0389
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-05-09 12:57:37 +0530

    Add webpack configuration and output options to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      {
+        test: /\.md$/,
+        // This is the asset module.
+        type: 'asset/source',
+      }
+    )
+    return config
+  },
+
+  output:process.env.HOST !=="GitHub" ? "standalone" : "export",
+  basePath:process.env.HOST !== "GitHub" ? "" : "/tour"
+  
+
+};
 
 export default nextConfig;
