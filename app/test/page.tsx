643fa9bd03f84bb8adb9f066881ccbb629365e81
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-05-11 23:35:45 +0530

    chore: read metadata from mdx
    there is still one problem, typescript is not identifying the metadata

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import Page1 from "@/content/01-introduction/01-why-json-schema.mdx";
+import Page1, {
+  metadata,
+} from "@/content/01-introduction/01-why-json-schema.mdx";
 
 export default function Test() {
+  console.log(metadata);
   return (
     <div>
       <Page1 />
--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@
 import createMDX from "@next/mdx";
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
-/** @type {import('next').NextConfig} */
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
-	remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+	remarkPlugins: [remarkFrontmatter, [remarkMdxFrontmatter,{name:"metadata"}]],
+	
   },
 });
 
