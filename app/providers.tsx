f27bc4a6d661e9ec088bb51d110ff1551f0b6806
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-05-11 20:35:56 +0530

    fix: set CSSReset to false to support mdx

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,5 +3,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider resetCSS={false} theme={theme}>
+      {children}
+    </ChakraProvider>
+  );
 }
