20a1a2e391b4bb6a1fdadb333aff92cb5773d4c9
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-05-11 22:27:48 +0530

    setup MDX
    resolves #11

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Page1 from "@/content/01-introduction/01-why-json-schema.mdx";
+
+export default function Test() {
+  return (
+    <div>
+      <Page1 />
+    </div>
+  );
+}
