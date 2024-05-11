7e058369e3e03ec658f4dea58d34dd67e4aea89e
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-05-11 16:26:56 +0530

    WIP

--- app/crawler/page.tsx
+++ app/crawler/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { listFiles, traverseDirectory } from "@/lib/fileUtils";
-
+import { title } from "@/content/01-introduction/01-why-json-schema.mdx";
 export default function Crawler() {
   const path = "./content";
   const files = traverseDirectory(path);
   console.log(files[0].children[0].children);
-  return <div>Crawler</div>;
+  return <div>{title}</div>;
 }
--- lib/crawler.ts
+++ lib/crawler.ts
@@ -1,8 +1,11 @@
-class Crawler {
-  constructor(contentFolderPath?: string) {
-    if (!contentFolderPath) {
-      contentFolderPath = "content";
-    }
-    console.log("Crawler constructor");
+// the content is stored in the content folder
+//
+
+class Content {
+  private contentFolderPath: string = "content";
+  constructor() {}
+
+  public crawl() {
+    return "Crawling content";
   }
 }
