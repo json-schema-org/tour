7c374ce6c42de580af6394c4f96680d5cbe897d8
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-05-11 22:40:41 +0530

    add remarkFrontmatter
    resolves #11

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 import createMDX from "@next/mdx";
-
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 /** @type {import('next').NextConfig} */
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
+  options: {
+	remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+  },
 });
 
 // Merge MDX config with Next.js config
