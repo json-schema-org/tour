452b62ca3b56a5fbdb52293214c5015feb1dbfd9
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-05-12 22:15:53 +0530

    remove logic to calculate next and previous steps

--- lib/contentManager.ts
+++ lib/contentManager.ts
@@ -86,58 +86,10 @@ export default class ContentManager {
             `${file.name}/${chapterFile.name}`
           );
 
-          let nextChapterIndex = 0;
-          let previousChapterIndex = 0;
-          let previousStepIndex = 0;
-
-          const maxSteps = chapterFiles.length;
-          const maxChapters = files.length;
-
-          // Calculate the nextStepIndex
-          let nextStepIndex = stepNumber + 1;
-          if (nextStepIndex >= maxSteps) {
-            // If the next step index exceeds the maximum steps in the current chapter,
-            // move to the next chapter if possible
-            let nextChapterIndex = chapterNumber + 1;
-            if (nextChapterIndex < maxChapters) {
-              nextStepIndex = 0; // Set the next step index to the first step of the next chapter
-            } else {
-              nextChapterIndex = chapterNumber; // Stay in the current chapter if it's the last chapter
-              nextStepIndex = stepNumber; // Stay at the current step
-            }
-          }
-
-          // Calculate the previousStepIndex
-          previousStepIndex = stepNumber - 1;
-          if (previousStepIndex < 0) {
-            // If the previous step index is less than 0,
-            // move to the previous chapter if possible
-            let previousChapterIndex = chapterNumber - 1;
-            if (previousChapterIndex >= 0) {
-              // Calculate the maximum number of steps for the previous chapter
-              const maxPreviousSteps =
-                fs.readdirSync(
-                  this.contentFolderName +
-                    "/" +
-                    files[previousChapterIndex].name
-                ).length - 1;
-              previousStepIndex = maxPreviousSteps - 1; // Set the previous step index to the last step of the previous chapter
-            } else {
-              previousChapterIndex = chapterNumber; // Stay in the current chapter if it's the first chapter
-              previousStepIndex = stepNumber; // Stay at the current step
-            }
-          }
-
-          const nextStepFileName = fs.readdirSync(
-            this.contentFolderName + "/" + files[nextChapterIndex].name
-          )[nextStepIndex];
-
           const step = {
             title: metadata.title,
             fileName: chapterFile.name,
             fullPath: `${file.name}/${chapterFile.name}`,
-            nextStepFullPath: `${files[nextChapterIndex].name}/${0}`,
-            // previousStepFullPath: `${files[previousChapterIndex].name}/${chapterFiles[previousStepIndex].name}`,
           };
           chapter.steps.push(step);
         });
