a683aa259aafe9fb54df8807c081b2c7539b2064
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-05-13 23:25:18 +0530

    Add dynamic code execution in CodeEditor component

--- app/components/CodeEditor/CodeEditor.tsx
+++ app/components/CodeEditor/CodeEditor.tsx
@@ -1,10 +1,21 @@
 import { contentManager } from "@/lib/contentManager";
+import fs from "fs";
 
 export default function CodeEditor({ mdPath }: { mdPath: string }) {
-  //   const { metadata } = contentManager.parseMdxFile(mdPath);
-  //   const folderName = contentManager.contentFolderName;
-  //   const file = await import(`./../../../${folderName}/${mdPath}`);
-  //   console.log(file.code);
+  const folderName = contentManager.contentFolderName;
+  const path = `./${folderName}/${mdPath}`.replace(".mdx", ".ts");
+  console.log(path);
+  const fileContent = fs.readFileSync(path, "utf-8");
+  console.log(fileContent);
+  const dynmicFunction = new Function("module", fileContent);
+  const moduleExports = {};
+  dynmicFunction(moduleExports);
+  moduleExports.exports.validationLogicFunction();
+  console.log(moduleExports);
+  //   const ast = parser.parse(fileContent, {
+  //     sourceType: "module",
+  //     plugins: ["jsx"],
+  //   });
 
   return <div>CodeEditor</div>;
 }
