3d870c5f45fd1813fec3de4b9b1a90aced3096bd
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-05-28 13:54:40 +0530

    Finish the step two

--- content/01-introduction/02-Arrays/code.ts
+++ content/01-introduction/02-Arrays/code.ts
@@ -1,5 +1,80 @@
-const code = {};
+const code: any = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+    },
+    age: {
+      type: "number",
+    },
+  },
+};
 
+const validationSchema = {
+  type: "object",
+  properties: {
+    type: {
+      type: "string",
+      const: "object",
+    },
+    properties: {
+      type: "object",
+      properties: {
+        name: {
+          type: "object",
+          properties: {
+            type: {
+              type: "string",
+              const: "string",
+            },
+          },
+          required: ["type"],
+        },
+        age: {
+          type: "object",
+          properties: {
+            type: {
+              type: "string",
+              const: "number",
+            },
+          },
+          required: ["type"],
+        },
+        skills: {
+          type: "object",
+          properties: {
+            type: {
+              type: "string",
+              const: "array",
+            },
+            items: {
+              type: "object",
+              properties: {
+                type: {
+                  type: "string",
+                  const: "string",
+                },
+              },
+              required: ["type"],
+            },
+          },
+        },
+      },
+      required: ["name", "age", "skills"],
+    },
+  },
+  required: ["type", "properties"],
+};
+
+const solution = structuredClone(code);
+solution.properties.skills = {
+  type: "array",
+  items: {
+    type: "string",
+  },
+};
 module.exports = {
   code,
+  solution,
+  validationSchema,
 };
