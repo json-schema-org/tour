31e4545e306adfacb8b1601f634d925ebe9785b5
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-06-01 21:41:42 +0530

    WIP testCasetabs

--- app/components/Output/Output.tsx
+++ app/components/Output/Output.tsx
@@ -1,44 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Output.module.css";
 import classnames from "classnames";
 import { FailedTestCase, OutputResult } from "@/lib/types";
-import CodeSnippet from "../CodeSnippet/CodeSnippet";
+
+function TestCaseTab({
+  isActive,
+  index,
+  setIsActive,
+}: {
+  isActive: boolean;
+  index: number;
+  setIsActive: (index: number) => void;
+}) {
+  return (
+    <div
+      className={classnames(
+        styles.testCaseTab,
+        isActive && styles.activeTestCaseTab
+      )}
+      onClick={() => setIsActive(index)}
+    >
+      Case {index + 1}
+    </div>
+  );
+}
+
+function FailedTestCaseItem({
+  testCase,
+  index,
+}: {
+  testCase: FailedTestCase;
+  index: number;
+}) {
+  return (
+    <div className={styles.failedTestCase}>
+      <div className={styles.failedTestCaseBody}>
+        <span className={styles.failedTestCaseTitle}>Expected Output:</span>
+        <span>{testCase.expected ? "valid" : "invalid"}</span>
+        <span className={styles.failedTestCaseTitle}>Actual Output:</span>
+        <span>{testCase.actual ? "valid" : "invalid"}</span>
+        <br />
+        <div className={styles.failedTestCaseCode}>
+          {JSON.stringify(testCase.input, null, 2)}
+        </div>
+        <div className={styles.failedTestCaseError}>
+          {testCase.errors &&
+            testCase.errors.map((i) => {
+              return i.keywordLocation;
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function FailedTestCasesWindow({
   failedTestCases,
+  totalTestCases,
 }: {
   failedTestCases: FailedTestCase[];
+  totalTestCases: number;
 }) {
+  const [activeTestCase, setActiveTestCase] = useState(0);
   return (
     <div className={styles.failedTestCasesWindow}>
-      <div className={styles.failedTestCasesHeader}>
-        Your Schema is Invalid.
+      <div className={styles.failedTestCasesHeaderWrapper}>
+        <span className={styles.failedTestCasesHeader}>Invalid Schema!</span>
+        <span className={styles.failedTestCasesSubtitle}>
+          {failedTestCases.length} out of {totalTestCases} test cases failed
+        </span>
       </div>
       <div className={styles.failedTestCasesBody}>
-        {failedTestCases.map((testCase, index) => (
-          <div key={index} className={styles.failedTestCase}>
-            <div className={styles.failedTestCaseHeader}>
-              Test Case {index + 1}
-            </div>
-            <div className={styles.failedTestCaseBody}>
-              <div className={styles.failedTestCaseTitle}>Input</div>
-              <div className={styles.failedTestCaseInput}>
-                <code>{JSON.stringify(testCase.input, null, 2)}</code>
-              </div>
-              <div className={styles.failedTestCaseTitle}>Expected Output</div>
-              <div className={styles.failedTestCaseExpectedOutput}>
-                {testCase.expected.toString()}
-              </div>
-              <div className={styles.failedTestCaseTitle}>Actual Output</div>
-              <div className={styles.failedTestCaseActualOutput}>
-                {testCase.actual.toString()}
-              </div>
-              <div className={styles.failedTestCaseError}>
-                {testCase.errors}
-              </div>
-            </div>
-          </div>
-        ))}
+        <div className={styles.failedTestCasesTabs}>
+          {failedTestCases.map((_, i) => (
+            <TestCaseTab
+              key={i}
+              isActive={i === activeTestCase}
+              index={i}
+              setIsActive={setActiveTestCase}
+            />
+          ))}
+        </div>
+        <FailedTestCaseItem
+          index={activeTestCase}
+          testCase={failedTestCases[activeTestCase]}
+        />
       </div>
     </div>
   );
@@ -72,7 +118,10 @@ function Output({
     outputBodyContent = "Syntax Error:" + outputResult.errors;
   } else {
     outputBodyContent = (
-      <FailedTestCasesWindow failedTestCases={outputResult.failedTestCases!} />
+      <FailedTestCasesWindow
+        failedTestCases={outputResult.failedTestCases!}
+        totalTestCases={outputResult.totalTestCases!}
+      />
     );
     console.log(outputResult.failedTestCases);
   }
