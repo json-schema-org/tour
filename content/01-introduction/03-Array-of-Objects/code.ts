0782f95814d2224878bb0651727afd293708433e
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-06-02 15:43:11 +0530

    Add array of objects to code.ts

---
+++ content/01-introduction/03-Array-of-Objects/code.ts
@@ -0,0 +1,71 @@
+const code: any = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+    },
+    age: {
+      type: "number",
+    },
+  },
+};
+
+const solution = structuredClone(code);
+solution.properties.skills = {
+  type: "array",
+  items: {
+    type: "object",
+    properties: {
+      name: { type: "string" },
+      level: {
+        enum: ["beginner", "intermediate", "advanced"],
+      },
+    },
+  },
+};
+
+const testCases: any[] = [
+  {
+    input: {
+      name: "person",
+      age: 23,
+      skills: [
+        {
+          name: "JavaScript",
+          level: "beginner",
+        },
+        {
+          name: "TypeScript",
+          level: "intermediate",
+        },
+        {
+          name: "React",
+          level: "advanced",
+        },
+      ],
+    },
+    expected: true,
+  },
+  {
+    input: {
+      name: "person",
+      age: 23,
+      skills: [
+        {
+          name: "JavaScript",
+          level: "beginner",
+        },
+        {
+          name: "TypeScript",
+          level: "expert",
+        },
+      ],
+    },
+    expected: false,
+  },
+];
+module.exports = {
+  code,
+  solution,
+  testCases,
+};
