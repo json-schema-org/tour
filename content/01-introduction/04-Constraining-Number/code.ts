73531c9e4242d4251de5fcaa346228b7fa18d828
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-06-03 14:24:27 +0530

    add test cases for constraining numbers

--- content/01-introduction/04-Constraining-Number/code.ts
+++ content/01-introduction/04-Constraining-Number/code.ts
@@ -4,64 +4,152 @@ const code: any = {
     name: {
       type: "string",
     },
-    age: {
-      type: "number",
-    },
+    age: {},
+    dateOfBirth: {},
   },
-};
-
-const validationSchema = {
-  type: "object",
-  properties: {
-    type: {
-      type: "string",
-      const: "object",
-    },
-    properties: {
-      type: "object",
-      properties: {
-        name: {
-          type: "object",
-          properties: {
-            type: {
-              type: "string",
-              const: "string",
-            },
-          },
-          required: ["type"],
-        },
-        age: {
-          type: "object",
-          properties: {
-            type: {
-              type: "string",
-              const: "number",
-            },
-          },
-          required: ["type"],
-        },
-        role: {
-          type: "object",
-          properties: {
-            const: {
-              const: "employee",
-            },
-          },
-          required: ["const"],
-        },
-      },
-      required: ["name", "age", "role"],
-    },
-  },
-  required: ["type", "properties"],
 };
 
 const solution = structuredClone(code);
-solution.properties.role = {
-  const: "employee",
+solution.properties.age = {
+  type: "number",
+  minimum: 18,
+  maximum: 60,
 };
+
+solution.properties.dateOfBirth = {
+  type: "object",
+  properties: {
+    year: {
+      type: "number",
+      minimum: 1964,
+      maximum: 2024,
+    },
+    month: {
+      type: "number",
+      minimum: 1,
+      maximum: 12,
+    },
+    day: {
+      type: "number",
+      minimum: 1,
+      maximum: 31,
+    },
+  },
+};
+
+const testCases: any[] = [
+  {
+    input: {
+      name: "person",
+      age: 23,
+      dateOfBirth: {
+        year: 1998,
+        month: 5,
+        day: 12,
+      },
+    },
+    expected: true,
+  },
+  {
+    input: {
+      name: "person",
+      age: 23,
+      dateOfBirth: {
+        year: 2025,
+        month: 5,
+        day: 12,
+      },
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "person",
+      age: 23,
+      dateOfBirth: {
+        year: 2024,
+        month: 0,
+        day: 12,
+      },
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "person",
+      age: 23,
+      dateOfBirth: {
+        year: 2024,
+        month: 13,
+        day: 12,
+      },
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "person",
+      age: 23,
+      dateOfBirth: {
+        year: 2024,
+        month: 12,
+        day: 0,
+      },
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "person",
+      age: 23,
+      dateOfBirth: {
+        year: 2024,
+        month: 12,
+        day: 32,
+      },
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "person",
+      age: 23,
+      dateOfBirth: {
+        year: 1963,
+        month: 5,
+        day: 12,
+      },
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "person",
+      age: 17,
+      dateOfBirth: {
+        year: 1998,
+        month: 5,
+        day: 12,
+      },
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "person",
+      age: 61,
+      dateOfBirth: {
+        year: 1998,
+        month: 5,
+        day: 12,
+      },
+    },
+    expected: false,
+  },
+];
+
 module.exports = {
   code,
   solution,
-  validationSchema,
+  testCases,
 };
