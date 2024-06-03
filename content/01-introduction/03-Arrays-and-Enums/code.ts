d0b8b279e3f420d7bd19cd6d1097c3c7458b0979
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-06-03 23:15:02 +0530

    docs: add arrays and enums section
    work in progress

---
+++ content/01-introduction/03-Arrays-and-Enums/code.ts
@@ -0,0 +1,46 @@
+const code: any = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+    },
+    age: {
+      type: "number",
+    },
+    hobbies: {},
+  },
+};
+
+const solution = structuredClone(code);
+solution.properties.hobbies = {
+  type: "array",
+  items: {
+    type: "string",
+    enum: ["reading", "writing", "painting"],
+  },
+};
+
+const testCases: any[] = [
+  {
+    input: {
+      name: "person",
+      age: 23,
+      hobbies: ["reading", "writing", "painting"],
+    },
+    expected: true,
+  },
+  {
+    input: {
+      name: "person",
+      age: 23,
+      hobbies: ["reading", "writing", "painting", "fighting"],
+    },
+    expected: false,
+  },
+];
+
+module.exports = {
+  code,
+  solution,
+  testCases,
+};
