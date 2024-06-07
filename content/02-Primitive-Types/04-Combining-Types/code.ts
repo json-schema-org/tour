6a1ee37f340b5a67250c164c11a895e1605202d4
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-06-07 22:51:58 +0530

    docs(refactor): move combining types

---
+++ content/02-Primitive-Types/04-Combining-Types/code.ts
@@ -0,0 +1,66 @@
+const code: any = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+    },
+    age: {
+      type: "integer",
+    },
+    hasAgreedToTerms: {},
+  },
+};
+
+const solution = structuredClone(code);
+solution.properties.hasAgreedToTerms = {
+  type: ["boolean", "null"],
+};
+
+const testCases: any[] = [
+  {
+    input: {
+      name: "person",
+      age: 23,
+      hasAgreedToTerms: true,
+    },
+    expected: true,
+  },
+  {
+    input: {
+      name: "person",
+      age: 23,
+      hasAgreedToTerms: null,
+    },
+    expected: true,
+  },
+  {
+    input: {
+      name: "person",
+      age: 23,
+      hasAgreedToTerms: "yes",
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: null,
+      age: 23,
+      hasAgreedToTerms: null,
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "person",
+      age: "23",
+      hasAgreedToTerms: null,
+    },
+    expected: false,
+  },
+];
+
+module.exports = {
+  code,
+  solution,
+  testCases,
+};
