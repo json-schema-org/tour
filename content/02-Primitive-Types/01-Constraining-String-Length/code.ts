77d97282160b9639b9f5ac79f19e782752ce6eb1
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-06-07 13:21:14 +0530

    docs: add constraing string length step

---
+++ content/02-Primitive-Types/01-Constraining-String-Length/code.ts
@@ -0,0 +1,86 @@
+const code: any = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+    },
+    age: {
+      type: "integer",
+    },
+  },
+};
+
+const testCases = [
+  {
+    input: {
+      name: "John Doe",
+      age: 30,
+      postalCode: "123456",
+      phoneNumber: "1234567890",
+    },
+    expected: true,
+  },
+  {
+    input: {
+      name: "John Doe",
+      age: 30,
+      postalCode: "1234567",
+      phoneNumber: "1234567890",
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "John Doe",
+      age: 30,
+      postalCode: "12345",
+      phoneNumber: "1234567890",
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "John Doe",
+      age: 30,
+      postalCode: "123456",
+      phoneNumber: "123456789",
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "John Doe",
+      age: "30",
+      postalCode: "123456",
+      phoneNumber: "1234567890",
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: 0,
+      age: 30,
+      postalCode: "123456",
+      phoneNumber: "1234567890",
+    },
+    expected: false,
+  },
+];
+
+const solution = structuredClone(code);
+solution.properties.phoneNumber = {
+  type: "string",
+  minLength: 10,
+};
+
+solution.properties.postalCode = {
+  type: "string",
+  minLength: 6,
+  maxLength: 6,
+};
+
+module.exports = {
+  code,
+  solution,
+  testCases,
+};
