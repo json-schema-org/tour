218b002365f280075cde806a0a1dcbaeac71a292
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-06-24 13:59:42 +0530

    chore: add adopted logos footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,60 @@
+"use client";
+
 import styles from "./styles/page.module.css";
 import Link from "next/link";
 import cx from "classnames";
-import { outfitFont } from "./styles/fonts";
+import { interFont, outfitFont } from "./styles/fonts";
 import RightArrow from "./styles/icons/rightArrow";
+import { useMemo } from "react";
+import { useColorMode } from "@chakra-ui/react";
+function CompanyLogos() {
+  const { colorMode } = useColorMode();
+
+  const logoInfo = useMemo(() => {
+    const logos: {
+      [key: string]: string;
+    } = {};
+    if (colorMode == "dark") {
+      logos["postman"] = "postman-dark";
+      logos["microsoft"] = "microsoft-dark";
+      logos["github"] = "github-dark";
+      logos["zapier"] = "zapier-dark";
+    } else {
+      logos["postman"] = "postman";
+      logos["microsoft"] = "microsoft";
+      logos["github"] = "github";
+      logos["zapier"] = "zapier";
+    }
+
+    return [
+      {
+        logo: logos["postman"],
+        height: "24px",
+        width: "79.08px",
+      },
+      { logo: logos["microsoft"], height: "18px", width: "77.38px" },
+      { logo: logos["github"], height: "18px", width: "59.14px" },
+      { logo: logos["zapier"], height: "18px", width: "66.57px" },
+    ];
+  }, [colorMode]);
+  return (
+    <div className={styles.footerLogos}>
+      {logoInfo.map((info) => (
+        <img
+          key={info.logo}
+          src={`/logos/${info.logo}.png`}
+          alt={info.logo}
+          className={styles.companyLogo}
+          style={{
+            height: info.height,
+            width: info.width,
+          }}
+        />
+      ))}
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div className={cx(styles.main, outfitFont.className)}>
@@ -27,6 +79,12 @@ export default function Home() {
             <RightArrow />
           </button>
         </Link>
+      </div>
+      <div className={styles.footer}>
+        <div className={cx(styles.footerText, interFont.className)}>
+          Adopted by
+        </div>
+        <CompanyLogos />
       </div>
     </div>
   );
--- app/styles/fonts.ts
+++ app/styles/fonts.ts
@@ -1,5 +1,9 @@
-import { Outfit } from "next/font/google";
+import { Outfit, Inter } from "next/font/google";
 
 export const outfitFont = Outfit({
+  subsets: ["latin"],
+});
+
+export const interFont = Inter({
   subsets: ["latin"],
 });
