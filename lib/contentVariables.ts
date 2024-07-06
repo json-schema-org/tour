2ae8c7f18d89f125a104667dcad2ab2d938fa2ac
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-07-06 23:46:15 +0530

    chore: add comment in the contentVariables.ts

--- lib/contentVariables.ts
+++ lib/contentVariables.ts
@@ -1,3 +1,5 @@
+// variables to generate the outline.json and iterate through the content
+
 export const contentFolderPath: string = "./content";
 export const contentFolderName: string = contentFolderPath.replace("./", "");
 export const indexFileName = "index.mdx";
