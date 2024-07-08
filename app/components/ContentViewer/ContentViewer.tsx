2c97dd164a1ab3d33d52ced5116cba10555e2d83
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-07-08 13:49:48 +0530

    refactor: remove useless imports

--- app/components/ContentViewer/ContentViewer.tsx
+++ app/components/ContentViewer/ContentViewer.tsx
@@ -1,9 +1,4 @@
-"use client";
-
 import styles from "./ContentViewer.module.css";
-import Progressbar from "../Progressbar";
-import { useEffect, useState } from "react";
-import { contentManager } from "@/lib/contentManager";
 
 export default function ContentViewer({
   children,
