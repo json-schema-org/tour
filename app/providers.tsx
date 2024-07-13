fa11bb5b93a4572c6fb69fb8284eeac1dd8bcb59
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-07-13 23:42:05 +0530

    remove colormode script

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
