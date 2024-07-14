2ffaefeebd764b019ed56060902f929062f126e7
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-07-14 17:58:39 +0530

    docs: add mutual dependency

---
+++ content/05-Conditional-Validation/02-Ensuring-Mutual-Dependency/code.ts
@@ -0,0 +1,70 @@
+const code: any = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+    },
+    creditCardNumber: {
+      type: "string",
+    },
+    address: {
+      type: "string",
+    },
+  },
+
+  dependentRequired: {
+    address: [],
+  },
+  required: ["name"],
+};
+
+const solution = structuredClone(code);
+solution.dependentRequired = {
+  creditCardNumber: ["address"],
+  address: ["creditCardNumber"],
+};
+const testCases = [
+  {
+    input: {
+      name: "John Doe",
+      creditCardNumber: "1234 5678 9012 3456",
+      address: "123 Main St",
+    },
+    expected: true,
+  },
+  {
+    input: {
+      name: "John Doe",
+      creditCardNumber: 1234,
+      address: "123 Main St",
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "John Doe",
+      creditCardNumber: "1234 5678 9012 3456",
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "John Doe",
+
+      address: "123 Main St",
+    },
+    expected: false,
+  },
+  {
+    input: {
+      address: "123 Main St",
+    },
+    expected: false,
+  },
+];
+
+module.exports = {
+  code,
+  solution,
+  testCases,
+};
