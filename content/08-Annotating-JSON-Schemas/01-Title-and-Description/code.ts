87fbdabf7b7380424001f21f60c889489b7dc568
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-07-29 22:14:48 +0530

    docs: title and description

---
+++ content/08-Annotating-JSON-Schemas/01-Title-and-Description/code.ts
@@ -0,0 +1,62 @@
+const code: any = {
+  type: "object",
+  properties: {
+    name: { type: "string" },
+    age: { type: "integer" },
+  },
+};
+
+let solution = structuredClone(code);
+solution.title = "Employee";
+solution.description = "An employee in the system";
+
+const testCases = [
+  {
+    input: {
+      name: "John Doe",
+      age: 30,
+    },
+    expected: true,
+  },
+  {
+    input: {
+      name: "John Doe",
+      age: 30.5,
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "John Doe",
+      age: "30",
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: 30,
+    },
+    expected: false,
+  },
+  {
+    input: {
+      name: "John Doe",
+    },
+    expected: true,
+  },
+  {
+    input: {
+      name: "John Doe",
+      age: 30.0,
+      location: "New York",
+    },
+    expected: true,
+  },
+];
+const expectedAnnotations = ["title", "description"];
+module.exports = {
+  code,
+  solution,
+  testCases,
+  expectedAnnotations,
+};
