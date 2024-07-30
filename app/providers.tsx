03f75453141427f35a57b26d50891278a864ea31
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-07-30 21:56:27 +0530

    add styles for mobile
    closes #48

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,14 @@
 "use client";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
+import { useEffect } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if (window.innerWidth < 768 && !window.location.href.includes("/mobile")) {
+      window.location.href = "/mobile";
+    }
+  }, []);
+
   return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
