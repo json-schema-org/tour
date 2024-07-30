ec1d687646cd6ff3d3ce6bff747dad5f0c787d99
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-07-30 20:24:18 +0530

    docs: id and Schema

--- content/06-Combining-Schemas/02-id-and-schema/code.ts
+++ content/06-Combining-Schemas/02-id-and-schema/code.ts
@@ -1,14 +1,22 @@
 const code: any = {
+  $schema: "https://json-schema.org/draft/2020-12/schema",
   type: "object",
   properties: {
-    name: { type: "string" },
+    name: {},
     age: { type: "integer" },
   },
 };
 
+let externalSchema = {
+  $id: "https://example.com/string",
+  $schema: "https://json-schema.org/draft/2020-12/schema",
+  type: "string",
+};
+
 let solution = structuredClone(code);
-solution.title = "Employee";
-solution.description = "An employee in the system";
+solution.properties.name = {
+  $ref: "https://example.com/string",
+};
 
 const testCases = [
   {
@@ -58,4 +66,5 @@ module.exports = {
   code,
   solution,
   testCases,
+  externalSchema,
 };
