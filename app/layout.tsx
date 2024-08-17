5937ee5111eaa9efab52d5f983d90801d9e878b7
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-08-17 19:43:00 +0530

    chore: remove the head tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head></head>
       <GoogleAnalytics gaId="G-X3SVRNR6WN" />
       <body className={GeistSans.className}>
         <Providers>
