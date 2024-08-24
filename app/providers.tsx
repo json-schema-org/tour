9e480631adfc09819c238cac235195c39c7ba39d
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-08-24 13:40:37 +0530

    chore: turn off mobile redirect (temporarily)
    related to #47

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,11 +4,11 @@ import { theme } from "./styles/theme";
 import { useEffect } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    if (window.innerWidth < 768 && !window.location.href.includes("/mobile")) {
-      window.location.href = "/mobile";
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (window.innerWidth < 768 && !window.location.href.includes("/mobile")) {
+  //     window.location.href = "/mobile";
+  //   }
+  // }, []);
 
   return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
