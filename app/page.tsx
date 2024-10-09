1d2be6426f93e108b6d220242ef78c8f9eed9f94
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-10-09 22:12:40 +0530

    refactor: styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,30 @@ import styles from "./styles/page.module.css";
 import cx from "classnames";
 import { interFont, outfitFont } from "./styles/fonts";
 import CompanyLogos from "./components/CommunityLinks/CommunityLinks";
-import HomePage from "./components/HomePageLinks/HomePageLinks";
+import HomePageLinks from "./components/HomePageLinks/HomePageLinks";
+import { Flex } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <div className={cx(styles.main, outfitFont.className)}>
-      <div className={styles.background}></div>
       <div className={styles.wrapper}>
-        <div className={styles.titleWrapper}>
-          <div className={styles.title}>
-            <div>A Tour of</div>
-            <div className={styles.jsonSchemaTitle}>JSON Schema</div>
-          </div>
-          <div className={styles.subtitleWrapper}>
-            <div className={styles.subtitle1}>
-              Build more. Break less. Empower others.
+        <div className={styles.backgroundClipWrapper}>
+          <div className={styles.titleWrapper}>
+            <div className={styles.title}>
+              <div>A Tour of</div>
+              <div className={styles.jsonSchemaTitle}>JSON Schema</div>
             </div>
-            <div className={styles.subtitle2}>
-              Ensure confident & reliable use of JSON data with JSON Schema
+            <div className={styles.subtitleWrapper}>
+              <div className={styles.subtitle1}>
+                Build more. Break less. Empower others.
+              </div>
+              <div className={styles.subtitle2}>
+                Ensure confident & reliable use of JSON data with JSON Schema
+              </div>
             </div>
           </div>
         </div>
-        <HomePage />
+        <HomePageLinks />
       </div>
       <div className={styles.footer}>
         <div className={cx(styles.footerText, interFont.className)}>
