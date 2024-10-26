74a87b103287462b3f4e0f2b42316ec6b8121fc9
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-10-26 14:20:43 +0500

    chnages

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,23 @@
+import Link from 'next/link'
 
-import Link from 'next/link';
-
-const Custom404 = () => {
+export default function Custom404() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-red-600 ">404</h1>
-      <p className="mt-4 text-xl text-gray-700">Sorry, the page you are looking for does not exist.</p>
-      <Link href="/" className='mt-4'>
-        <span className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
-          Go back home
-        </span>
-      </Link>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 p-4">
+      <div className="text-center">
+        <h1 className="text-8xl font-bold mb-8">
+          <span className="text-red-400 mr-2">{'{'}</span>
+          <span className="text-red-500">404</span>
+          <span className="text-gray-400 ml-2">{'}'}</span>
+        </h1>
+        <p className="text-xl text-gray-600 mb-8">
+          Oops! The page you{`'`}re looking for doesn{`'`}t exist.
+        </p>
+        <Link href="/" className="inline-block">
+          <button className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+            Back to Home
+          </button>
+        </Link>
+      </div>
     </div>
-  );
-};
-
-export default Custom404;
+  )
+}
