6c01fe45b30cda3dfe6f66ccc0fb95b7c6a7b4aa
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-10-29 16:37:26 +0500

    update the page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,23 +1,16 @@
-import Link from 'next/link'
+// pages/404.tsx
+
+import styles from './styles/404.module.css';
+import Link from 'next/link';
 
 export default function Custom404() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 p-4">
-      <div className="text-center">
-        <h1 className="text-8xl font-bold mb-8">
-          <span className="text-red-400 mr-2">{'{'}</span>
-          <span className="text-red-500">404</span>
-          <span className="text-gray-400 ml-2">{'}'}</span>
-        </h1>
-        <p className="text-xl text-gray-600 mb-8">
-          Oops! The page you{`'`}re looking for doesn{`'`}t exist.
-        </p>
-        <Link href="/" className="inline-block">
-          <button className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-            Back to Home
-          </button>
-        </Link>
-      </div>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>404 Not Found!</h1>
+            <p className={styles.message}>The page you are looking for does not exist!</p>
+            <Link href="/">
+                <span className={styles.button}>Back to Home</span>
+            </Link>
+        </div>
+    );
 }
