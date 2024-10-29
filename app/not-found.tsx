849af4dd5499a5ac73aff5e6041b27fb87c8d5e9
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-10-29 21:40:11 +0500

    update the design for both dark theme and light theme

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,34 @@
-// pages/404.tsx
-
+"use client";
+import { useColorMode } from '@chakra-ui/react';
 import styles from './styles/404.module.css';
 import Link from 'next/link';
 
 export default function Custom404() {
+    const { colorMode } = useColorMode();
+    const isLightMode = colorMode === 'light';
+
     return (
-        <div className={styles.container}>
+        <div
+            className={styles.container}
+            style={{
+                backgroundColor: isLightMode ? '#ffffff' : '#1a202c', // light mode background or dark mode
+                color: isLightMode ? '#000000' : '#ffffff' // text color based on mode
+            }}
+        >
             <h1 className={styles.title}>404 Not Found!</h1>
             <p className={styles.message}>The page you are looking for does not exist!</p>
             <Link href="/">
-                <span className={styles.button}>Back to Home</span>
+                <span
+                    className={styles.button}
+                    style={{
+                        color: isLightMode ? '#ffffff' : '#1a202c', // Button text color based on mode
+                        backgroundColor: isLightMode ? '#1e3a8a' : 'white' // Button background based on mode
+                    }}
+                >
+                    Back to Home
+                </span>
             </Link>
         </div>
     );
 }
+
