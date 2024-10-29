740a62a24b8a55b8ed152ff4b62c54bb0370c0b5
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-10-29 22:05:16 +0500

    use globall css colors instead of value

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,34 +1,23 @@
 "use client";
-import { useColorMode } from '@chakra-ui/react';
+import { useColorMode, useColorModeValue } from '@chakra-ui/react';
 import styles from './styles/404.module.css';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 export default function Custom404() {
     const { colorMode } = useColorMode();
-    const isLightMode = colorMode === 'light';
+    
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', colorMode);
+    }, [colorMode]);
 
     return (
-        <div
-            className={styles.container}
-            style={{
-                backgroundColor: isLightMode ? '#ffffff' : '#1a202c', // light mode background or dark mode
-                color: isLightMode ? '#000000' : '#ffffff' // text color based on mode
-            }}
-        >
+        <div className={styles.container}>
             <h1 className={styles.title}>404 Not Found!</h1>
             <p className={styles.message}>The page you are looking for does not exist!</p>
             <Link href="/">
-                <span
-                    className={styles.button}
-                    style={{
-                        color: isLightMode ? '#ffffff' : '#1a202c', // Button text color based on mode
-                        backgroundColor: isLightMode ? '#1e3a8a' : 'white' // Button background based on mode
-                    }}
-                >
-                    Back to Home
-                </span>
+                <span className={styles.button}>Back to Home</span>
             </Link>
         </div>
     );
 }
-
