851bec3bcd1686fb2e231edbe5219f02badc4b9b
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-10-30 20:26:17 +0500

    improve the style using figma values

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,16 @@
-"use client";
-import { useColorMode, useColorModeValue } from '@chakra-ui/react';
 import styles from './styles/404.module.css';
+import cx from 'classnames';
+import { interFont } from "./styles/fonts";
 import Link from 'next/link';
-import { useEffect } from 'react';
 
 export default function Custom404() {
-    const { colorMode } = useColorMode();
     
-    useEffect(() => {
-        document.documentElement.setAttribute('data-theme', colorMode);
-    }, [colorMode]);
 
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>404 Not Found!</h1>
+            <h1 className={cx(styles.title,interFont.className)}>404 Not Found!</h1>
             <p className={styles.message}>The page you are looking for does not exist!</p>
-            <Link href="/">
+            <Link href="/" className={styles.btnContainer}>
                 <span className={styles.button}>Back to Home</span>
             </Link>
         </div>
