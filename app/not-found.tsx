8c5b6d8777999f74c5bf9e9db1eb96ce18c369f5
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-11-02 15:38:03 +0530

    refactor: styles and containers

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,25 @@
-import styles from './styles/404.module.css';
-import cx from 'classnames';
+import styles from "./styles/404.module.css";
+import cx from "classnames";
 import { interFont } from "./styles/fonts";
-import Link from 'next/link';
+import Link from "next/link";
+import { Button } from "@chakra-ui/react";
 
 export default function Custom404() {
-    
-
-    return (
-        <div className={styles.container}>
-            <h1 className={cx(styles.title,interFont.className)}>404 Not Found!</h1>
-            <p className={styles.message}>The page you are looking for does not exist!</p>
-            <Link href="/" className={styles.btnContainer}>
-                <span className={styles.button}>Back to Home</span>
-            </Link>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1 className={cx(styles.title, interFont.className)}>
+          404 Not Found!
+        </h1>
+        <p className={styles.message}>
+          The page you are looking for does not exist!
+        </p>
+      </div>
+      <Link href={"/"}>
+        <Button variant={"default"} borderRadius={"8px"}>
+          BACK TO HOME
+        </Button>
+      </Link>
+    </div>
+  );
 }
