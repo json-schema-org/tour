8ef89603b504de7bdb8af198f2247a88de151da8
Me Priyank <pvonlyone29@gmail.com>
2025-11-30 18:44:15 +0530

    Revert "Fix: #138 - Implement mobile responsiveness for the tour page UI."
    This reverts commit 1910a58955d7d266345f900bc6a9509b9001e4cd.

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,56 +1,14 @@
 "use client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
-import styles from "./styles/page.module.css";
-import classnames from "classnames";
-
-import { useEffect, useState } from "react";
-import { outfitFont } from "./styles/fonts";
-
-const MOBILE_BREAKPOINT = 768;
+import { useEffect } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  // 1. Initialize state for mobile status
-  const [isMobile, setIsMobile] = useState(false);
-  const [hasChecked, setHasChecked] = useState(false);
+  // useEffect(() => {
+  //   if (window.innerWidth < 768 && !window.location.href.includes("/mobile")) {
+  //     window.location.href = "/mobile";
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    // 2. Function to check the screen width
-    const checkMobileStatus = () => {
-      setIsMobile(window.innerWidth <= MOBILE_BREAKPOINT);
-      setHasChecked(true);
-    };
-
-    checkMobileStatus();
-
-
-    window.addEventListener('resize', checkMobileStatus);
-
-    return () => {
-      window.removeEventListener('resize', checkMobileStatus);
-    };
-  }, []);
-
-  if (!hasChecked) {
-    return null;
-  }
-
-  if (!isMobile) {
-    return (
-      <ChakraProvider theme={theme}>
-        {children}
-      </ChakraProvider>
-    );
-  }
-
-  return (
-    <div className={styles.main}>
-      <div className={classnames(styles.message, outfitFont.className)}>
-        We are sorry,
-        <br /> 
-        Tour of JSON Schema is not optimized for mobile devices. Please use a
-        desktop computer for the best experience.
-      </div>
-    </div>
-  );
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
